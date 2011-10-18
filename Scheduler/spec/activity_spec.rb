80677126bd054a005f2617e29da68f389c39fd9e
WhatsMyDay <fs.khaldi@yahoo.co.uk>
2011-10-18 13:00:26 +0100

    to_string has created and it passes

--- Scheduler/spec/activity_spec.rb
+++ Scheduler/spec/activity_spec.rb
@@ -6,13 +6,23 @@ require '../lib/activity.rb'
 
 describe Activity do
 
+  it "should split the string into sub_strings" do
+    test_string = "1 | Doctor | 4:00 | 1:00 | appointment for check_up"
+    results =subject.decode_line(test_string)
+    results[0].should == "1"
+    results[1].should == "Doctor"
+    results[2].should == "4:00"
+    results[3].should == "1:00"
+    results[4].should == "appointment for check_up"
+
+  end
 
   it "should have an ID of 356"     do
 
 
-      activity = Activity.new(356,"hi",13/13/2011,"3 hours","Hi")
+    activity = Activity.new(356,"hi",13/13/2011,"3 hours","Hi")
 
-      activity.id.should == 356
+    activity.id.should == 356
 
   end
 
@@ -21,11 +31,8 @@ describe Activity do
 
   it "should list all activities" do
 
-                    activity = Activity.new(1324,"hi",13/13/2011,"3 hours","Hi")
-                    activity.list
-
-
-
+    activity = Activity.new(1324,"hi",13/13/2011,"3 hours","Hi")
+    activity.list
 
   end
 
