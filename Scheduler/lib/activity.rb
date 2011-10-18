996a51c76e784780e108ac9ec0aaa23aead4afbe
Colin Barrett <c.r.b.barrett@gmail.com>
2011-10-18 12:35:28 +0100

    decode_line method complete

--- Scheduler/lib/activity.rb
+++ Scheduler/lib/activity.rb
@@ -6,7 +6,7 @@ class Activity
 
 
 
-  def initialize(num, title, date , dur , desc )
+  def initialize(num = nil, title = nil, date = nil, dur = nil, desc = nil)
 
     @id = num
     @title = title
@@ -16,20 +16,21 @@ class Activity
 
   end
 
-
-
-
-
-end
-
   def decode_line input_string
     result = []
     input_string.split(" | ").each do | s |
       result << s.strip
     end
+    @id = result[0].to_i
+    @title = result[1]
+    m = /(\d\d)\/(\d\d)\/(\d\d\d\d)\s(\d\d):(\d\d)/.match(result[2])
+    @date = Time.mktime(m[3].to_i, m[2].to_i, m[1].to_i, m[4].to_i, m[5].to_i)
+    n = /(\d+):(\d\d)/.match(result[3])
+    @duration = (n[1].to_i * 60) + n[2].to_i
+    @description = result[4]
     result
   end
-
+end
 
 
 
--- Scheduler/spec/activity_spec.rb
+++ Scheduler/spec/activity_spec.rb
@@ -6,17 +6,26 @@ require '../lib/activity.rb'
 describe Activity do
 
   it "should split the string into sub_strings" do
-    test_string = "1 | Doctor | 4:00 | 1:00 | appointment for check_up"
-    results =subject.decode_line(test_string)
+    test_string = "1 | Doctor | 19/10/2011 16:00 | 1:00 | appointment for check_up"
+    results = subject.decode_line(test_string)
     results[0].should == "1"
     results[1].should == "Doctor"
-    results[2].should == "4:00"
+    results[2].should == "19/10/2011 16:00"
     results[3].should == "1:00"
     results[4].should == "appointment for check_up"
-
   end
 
-  it "should have an ID of 356"     do
+  it "should store the decoded strings in the class attributes" do
+    test_string = "1 | Doctor | 19/10/2011 16:00 | 1:00 | appointment for check_up"
+    subject.decode_line(test_string)
+    subject.id.should == 1
+    subject.title.should == "Doctor"
+    subject.date.should == Time.mktime(2011, 10, 19, 16, 0)
+    subject.duration.should == 60
+    subject.description.should == "appointment for check_up"
+  end
+
+  it "should have an ID of 356" do
     activity = Activity.new
     activity.id.should == 356
   end
