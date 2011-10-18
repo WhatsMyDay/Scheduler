a7168062f0ca166667e6b43d5331565a8fffee81
Colin Barrett <c.r.b.barrett@gmail.com>
2011-10-18 15:51:41 +0100

    load_from_file method created

--- Scheduler/lib/activityarray.rb
+++ Scheduler/lib/activityarray.rb
@@ -1,4 +1,13 @@
-class ActivityArray
+require 'activity'
 
-
+class ActivityArray < Array
+   def load_from_file file_name
+     f = File.open(file_name)
+     line = f.gets
+     puts line
+     f.close()
+     activity = Activity.new()
+     activity.decode_line(line)
+     self << activity
+   end
 end
--- Scheduler/spec/activityarray_spec.rb
+++ Scheduler/spec/activityarray_spec.rb
@@ -2,5 +2,21 @@ require "rspec"
 require '../lib/activityarray.rb'
 
 describe ActivityArray do
+  it "should be instantiated as an empty array" do
+    subject.length.should == 0
+  end
 
+  it "should load the activities from a file" do
+    f=File.new("testfile", "w")
+    f.write("100 | doctor | 23/01/2011 04:23 | 0:30 | general checkup\n")
+    f.close()
+
+    subject.load_from_file("testfile")
+    subject.length.should == 1
+    subject[0].id.should == 100
+    subject[0].title.should == "doctor"
+    subject[0].date.should == Time.mktime(2011,01,23,4,23)
+    subject[0].duration.should == 30
+    subject[0].description == "general checkup"
+  end
 end
