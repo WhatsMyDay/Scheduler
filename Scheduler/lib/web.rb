cd403706689cc09e83676c4cd67d0b1c6d236f57
Colin Barrett <c.r.b.barrett@gmail.com>
2011-10-22 14:52:17 +0100

    Priority added to the add_activity page

--- Scheduler/lib/web.rb
+++ Scheduler/lib/web.rb
@@ -5,10 +5,11 @@ require 'activityarray'
 array=ActivityArray.new
 
 get '/activities' do
-  #erb:activities
   result=""
   array.each do |activity|
-   result+=activity.to_s+" <a href=\"edit_activity/"+activity.id.to_s+"\">Edit</a>"
+    result += activity.to_s +
+        " <a href=\"edit_activity/"+activity.id.to_s+"\">Edit</a>" +
+        " <a href=\"delete_activity/" + activity.id.to_s + "\">Delete</a> <br>"
   end
   result
 end
@@ -22,11 +23,12 @@ post '/insert_activity' do
   date = params[:date]
   duration = params[:duration]
   description = params[:description]
-  m = /(\d\d)\/(\d\d)\/(\d\d\d\d)\s(\d\d):(\d\d)/.match(date)
-  start_time = Time.mktime(m[3].to_i, m[2].to_i, m[1].to_i, m[4].to_i, m[5].to_i)
-  activity = Activity.new(100, title, start_time, duration.to_i, description)
+  priority = params[:priority]
+  activity = Activity.new(100, title, date, duration.to_i, description, priority)
   array << activity
   array.store_to_file("saved_activities.txt")
+
+  redirect '/activities'
 end
 
 get '/edit_activity/:id' do
@@ -34,7 +36,6 @@ get '/edit_activity/:id' do
   @activity = activities.first
   erb :edit_activity
 end
-
 
 get '/delete_activity/:id' do
   array.delete_if { |a| a.id == params[:id].to_i }
