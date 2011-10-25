ff5b2d6e93a497426d5ef0a9a54d9a46fcd0b4c4
bamdad dashtban <dashtbab@cs.manchester.ac.uk>
2011-10-25 12:41:35 +0100

    functional testing of editing an activity

--- Scheduler/spec/web_test_spec.rb
+++ Scheduler/spec/web_test_spec.rb
@@ -6,16 +6,7 @@ require "rubygems"
 
 
 describe Activity do
-  #it "should fill the form and create an activity on web page" do
-  #  browser = Watir::Browser.new :ff
-  #  browser.goto "localhost:4567/add_activity"
-  #  browser.text_field(:name => "title").set "Doctor"
-  #  browser.text_field(:name => "date").set "16/10/2011 10:11:15"
-  #  browser.text_field(:name => "duration").set "30"
-  #  browser.text_field(:name => "description").set "Check up!"
-  #  browser.button(:value => "Create!").click
-  #  browser.url.should=="http://localhost:4567/insert_activity"
-  #end
+
   it "should create and edit an activity on web page" do
     browser = Watir::Browser.new :ff
     browser.goto "localhost:4567/add_activity"
@@ -29,6 +20,20 @@ describe Activity do
     browser.link(:text => "Edit").click
     browser.select_list(:id, "status").set("Done")
 
+  end
+
+  it "should edit an activity" do
+    browser = Watir::Browser.new :ff
+    browser.goto "localhost:4567/activities"
+    browser.link(:text => "Edit").click
+    browser.text_field(:name => "title").set "Dentist"
+    browser.text_field(:name => "date").set "16/10/2011 10:15"
+    browser.text_field(:name => "duration").set "20"
+    browser.text_field(:name => "description").set "Dental Checkup"
+    browser.select_list(:name => 'is_done').select 'Yes'
+    browser.radio(:value => "Urgent").set
+    browser.button(:value=> "Change").click
+    browser.url.should=="http://localhost:4567/activities"
   end
 
 end
