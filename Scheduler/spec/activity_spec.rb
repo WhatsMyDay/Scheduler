5c683e7f2080892854ca0b17b0439e6b1e283890
Colin Barrett <c.r.b.barrett@gmail.com>
2011-10-25 12:02:24 +0100

    Added location attribute to activity class

--- Scheduler/spec/activity_spec.rb
+++ Scheduler/spec/activity_spec.rb
@@ -3,7 +3,7 @@ require '../lib/activity.rb'
 
 describe Activity do
   it "should split the string into sub_strings" do
-    test_string = "1 | Doctor | 19/10/2011 16:00 | 01:00 | appointment for check_up | done | urgent"
+    test_string = "1 | Doctor | 19/10/2011 16:00 | 01:00 | appointment for check_up | done | urgent | Manchester"
     results = subject.decode_line(test_string)
     results[0].should == "1"
     results[1].should == "Doctor"
@@ -12,10 +12,11 @@ describe Activity do
     results[4].should == "appointment for check_up"
     results[5].should == "done"
     results[6].should == "urgent"
+    results[7].should == "Manchester"
   end
 
   it "should store the decoded strings in the class attributes" do
-    test_string = "1 | Doctor | 19/10/2011 16:00 | 01:00 | appointment for check_up | done | urgent"
+    test_string = "1 | Doctor | 19/10/2011 16:00 | 01:00 | appointment for check_up | done | urgent | Manchester"
     subject.decode_line(test_string)
     subject.id.should == 1
     subject.title.should == "Doctor"
@@ -24,29 +25,30 @@ describe Activity do
     subject.description.should == "appointment for check_up"
     subject.is_done.should == true
     subject.priority.should == "urgent"
+    subject.location.should == "Manchester"
   end
 
   it "should encode the class into the string" do
-    activity = Activity.new(1, "Doctor", "19/10/2011 16:00", 60, "appointment for check_up","low")
-    test_string = "1 | Doctor | 19/10/2011 16:00 | 01:00 | appointment for check_up | not done | low"
+    activity = Activity.new(1, "Doctor", "19/10/2011 16:00", 60, "appointment for check_up", "low", "Manchester")
+    test_string = "1 | Doctor | 19/10/2011 16:00 | 01:00 | appointment for check_up | not done | low | Manchester"
     activity.encode_line().should ==test_string
   end
 
   it "should have an ID of 356" do
-    activity = Activity.new(356, "Dentist", "19/10/2011 16:00", "01:00", "For X-Rays","normal")
+    activity = Activity.new(356, "Dentist", "19/10/2011 16:00", "01:00", "For X-Rays", "normal", "Manchester")
     activity.id.should == 356
   end
 
   it "should show the toString result" do
-    activity1 = Activity.new(1324, "Dental appointment", "19/10/2011 16:00", "3 hours", "Kilburn building", "urgent")
-    activity1.to_s.should=='1324: 19/10/2011 16:00 (3 hours) Dental appointment - Kilburn building > not done, urgent'
+    activity1 = Activity.new(1324, "Dental appointment", "19/10/2011 16:00", "3 hours", "Kilburn building", "urgent", "Manchester")
+    activity1.to_s.should=='19/10/2011 16:00 (3 hours) Dental appointment - Kilburn building > not done, urgent in Manchester'
   end
 
   it "should update the status" do
-     activity1 = Activity.new(1234,"Dental appointment", "19/10/2011 16:00", "3 hours", "Kilburn building", "urgent")
-     activity1.is_done = true
-     activity1.is_done.should == true
-     activity1.is_done = false
+    activity1 = Activity.new(1234, "Dental appointment", "19/10/2011 16:00", "3 hours", "Kilburn building", "urgent", "Manchester")
+    activity1.is_done = true
+    activity1.is_done.should == true
+    activity1.is_done = false
     activity1.is_done.should == false
   end
 end
--- Scheduler/spec/activityarray_spec.rb
+++ Scheduler/spec/activityarray_spec.rb
@@ -8,7 +8,7 @@ describe ActivityArray do
 
   it "should load one activities from a file" do
     f=File.new("testfile", "w")
-    f.write("100 | doctor | 23/01/2011 04:23 | 00:30 | general checkup | done | urgent\n")
+    f.write("100 | doctor | 23/01/2011 04:23 | 00:30 | general checkup | done | urgent | Manchester\n")
     f.close()
 
     subject.load_from_file("testfile")
@@ -20,13 +20,14 @@ describe ActivityArray do
     subject[0].description == "general checkup"
     subject[0].is_done == true
     subject[0].priority =="urgent"
+    subject[0].location == "Manchester"
   end
 
   it "should load several activities from a file" do
     f=File.new("testfile", "w")
-    f.write("100 | doctor | 23/01/2011 04:00 | 00:30 | general checkup | done | urgent\n")
-    f.write("200 | dentist | 24/02/2011 07:00 | 00:45 | see hygenist | done | urgent\n")
-    f.write("300 | optician | 25/03/2011 10:00 | 01:00 | mend glasses | done | urgent\n")
+    f.write("100 | doctor | 23/01/2011 04:00 | 00:30 | general checkup | done | urgent | Manchester\n")
+    f.write("200 | dentist | 24/02/2011 07:00 | 00:45 | see hygenist | done | urgent | Manchester\n")
+    f.write("300 | optician | 25/03/2011 10:00 | 01:00 | mend glasses | done | urgent | Manchester\n")
     f.close()
 
     subject.load_from_file("testfile")
@@ -35,41 +36,41 @@ describe ActivityArray do
     subject[0].title.should == "doctor"
     subject[0].start_time.to_s.should == '23/01/2011 04:00'
     subject[0].duration.should == 30
-    subject[0].description == "general checkup"
-    subject[0].is_done == true
-    subject[0].priority == "urgent"
+    subject[0].description.should == "general checkup"
+    subject[0].is_done.should == true
+    subject[0].priority.should == "urgent"
+    subject[0].location.should == "Manchester"
 
     subject[1].id.should == 200
     subject[1].title.should == "dentist"
     subject[1].start_time.to_s.should == '24/02/2011 07:00'
     subject[1].duration.should == 45
-    subject[1].description == "see hygenist"
-    subject[1].is_done == true
-    subject[1].priority == "urgent"
+    subject[1].description.should == "see hygenist"
+    subject[1].is_done.should == true
+    subject[1].priority.should == "urgent"
+    subject[1].location.should == "Manchester"
 
     subject[2].id.should == 300
     subject[2].title.should == "optician"
     subject[2].start_time.to_s.should == '25/03/2011 10:00'
     subject[2].duration.should == 60
-    subject[2].description == "mend glasses"
-    subject[2].is_done == true
-    subject[2].priority == "urgent"
+    subject[2].description.should == "mend glasses"
+    subject[2].is_done.should == true
+    subject[2].priority.should == "urgent"
+    subject[2].location.should == "Manchester"
   end
 
   it "should store several activities in a file" do
-    subject << Activity.new(100, "doctor", '23/01/2011 04:00', 30, "general checkup", "urgent")
-    subject << Activity.new(200, "dentist", '24/02/2011 07:00', 45, "see hygenist", "urgent")
-    subject << Activity.new(300, "optician", '25/03/2011 10:00', 60, "mend glasses", "urgent")
+    subject << Activity.new(100, "doctor", '23/01/2011 04:00', 30, "general checkup", "urgent", "Manchester")
+    subject << Activity.new(200, "dentist", '24/02/2011 07:00', 45, "see hygenist", "urgent", "Manchester")
+    subject << Activity.new(300, "optician", '25/03/2011 10:00', 60, "mend glasses", "urgent", "Manchester")
 
     subject.store_to_file("output_file.txt")
 
     f = File.open("output_file.txt")
-    f.gets.should == "100 | doctor | 23/01/2011 04:00 | 00:30 | general checkup | not done | urgent\n"
-    f.gets.should == "200 | dentist | 24/02/2011 07:00 | 00:45 | see hygenist | not done | urgent\n"
-    f.gets.should == "300 | optician | 25/03/2011 10:00 | 01:00 | mend glasses | not done | urgent\n"
+    f.gets.should == "100 | doctor | 23/01/2011 04:00 | 00:30 | general checkup | not done | urgent | Manchester\n"
+    f.gets.should == "200 | dentist | 24/02/2011 07:00 | 00:45 | see hygenist | not done | urgent | Manchester\n"
+    f.gets.should == "300 | optician | 25/03/2011 10:00 | 01:00 | mend glasses | not done | urgent | Manchester\n"
     f.close()
   end
-
-
-
 end
