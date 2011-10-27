4b9e2ad4104453daa553a26626d86d9d253d7179
Colin Barrett <c.r.b.barrett@gmail.com>
2011-10-27 17:07:19 +0100

    Refactor test specs by adding context

--- Scheduler/lib/web.rb
+++ Scheduler/lib/web.rb
@@ -6,9 +6,7 @@ array=ActivityArray.new
 
 get '/activities/?:condition?' do
   selection = params[:condition].nil? ? proc { true } : proc { |a| a.is_done == false }
-
   @selected_activities = array.select &selection
-
   erb :activities
 end
 
@@ -17,27 +15,24 @@ get '/add_activity' do
 
   erb :add_activity
 end
-
 post '/insert_activity' do
   activity = Activity.new(Time.now.to_i, params[:title], params[:start_time], params[:duration].to_i, params[:description],
                           params[:priority], params[:location], params[:notes])
   array << activity
-
   redirect '/activities'
 end
 
 get '/edit_activity/:id' do
   @activity = (array.select { |a| a.id == params[:id].to_i }).first
-
   erb :add_activity
 end
 
 
 get '/delete_activity/:id' do
   array.delete_if { |a| a.id == params[:id].to_i }
-
   redirect '/activities'
 end
+
 post '/do_edit_activity' do
   activities = array.select { |a| a.id == params[:id].to_i }
   activity = activities.first
@@ -49,7 +44,6 @@ post '/do_edit_activity' do
   activity.location = params[:location]
   activity.notes = params[:notes]
   activity.priority = params[:priority]
-
   redirect '/activities'
 end
 
